f52c263197f20f124f86667d0edb3da6c5cc2834
darpan <darpanneve3@gmail.com>
2023-01-13 19:51:46 +0530

    started to send message layout creation

--- app/src/main/java/com/example/adminpanel/NoticeActivity.java
+++ app/src/main/java/com/example/adminpanel/NoticeActivity.java
@@ -1,7 +1,11 @@
 package com.example.adminpanel;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -11,7 +15,10 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -22,6 +29,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -35,7 +43,7 @@ public class NoticeActivity extends AppCompatActivity {
     ImageView logout,logo,sendbutton;
     RecyclerView recyclerView;
     NoticeAdapter NoticeAdapter;
-    DatabaseReference mDatabase;
+    DatabaseReference DatabaseReference;
     EditText sendmessage;
     String message,name,email1;
     Date time;
@@ -43,9 +51,8 @@ public class NoticeActivity extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
     FirebaseUser firebaseUser;
     data data;
+    int id=0;
     FirebaseDatabase firebaseDatabase;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,6 +66,7 @@ public class NoticeActivity extends AppCompatActivity {
         firebaseAuth= FirebaseAuth.getInstance();
         firebaseUser=firebaseAuth.getCurrentUser();
         firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference=firebaseDatabase.getReference().child("chats");
         googleSignInClient= GoogleSignIn.getClient(this, GoogleSignInOptions.DEFAULT_SIGN_IN);
         FirebaseRecyclerOptions<NoticeModel> options
                 = new FirebaseRecyclerOptions.Builder<NoticeModel>()
@@ -68,9 +76,7 @@ public class NoticeActivity extends AppCompatActivity {
         LinearLayoutManager lm = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, true); // last argument (true) is flag for reverse layout
         recyclerView.setLayoutManager(lm);
         recyclerView.setAdapter(NoticeAdapter);
-
         data = new data();
-
         sendmessage.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -101,7 +107,6 @@ public class NoticeActivity extends AppCompatActivity {
                     message=sendmessage.getText().toString();
                     time= Calendar.getInstance().getTime();
                     name=email1;
-                    mDatabase = firebaseDatabase.getReference().child("chats").child(String.valueOf(time));
                     addDatatoFirebase(name,message,time);
 
                 }
@@ -114,20 +119,20 @@ public class NoticeActivity extends AppCompatActivity {
                 data.setMessage(message);
                 data.setTime(String.valueOf(time));
 
+                DatabaseReference.child(String.valueOf(id+1)).setValue(data);
 
                 // we are use add value event listener method
                 // which is called with database reference.
-                mDatabase.addValueEventListener(new ValueEventListener() {
+                DatabaseReference.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
                         // inside the method of on Data change we are setting
                         // our object class to our database reference.
                         // data base reference will sends data to firebase.
-                        mDatabase.setValue(data);
                         // after adding this data we are showing toast message.
                         Toast.makeText(NoticeActivity.this, "data added", Toast.LENGTH_SHORT).show();
                         sendmessage.setText("");
-
+                        sendnotification();
                     }
 
                     @Override
@@ -148,8 +153,59 @@ public class NoticeActivity extends AppCompatActivity {
             }
         });
 
+    }
 
+    private void sendnotification() {
+        DatabaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    id=(int)snapshot.getChildrenCount();
+                }
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        DatabaseReference.addChildEventListener(new ChildEventListener() {
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+                    NotificationChannel channel=new NotificationChannel("n","n", NotificationManager.IMPORTANCE_DEFAULT);
+                    NotificationManager manager=getSystemService(NotificationManager.class);
+                    manager.createNotificationChannel(channel);
+                }
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), "n");
+                builder.setSmallIcon(R.drawable.pccoelogo);
+
+                builder.setContentTitle(name);
+                builder.setAutoCancel(true);
+                builder.setContentText(message);
+                NotificationManagerCompat managerCompat=NotificationManagerCompat.from(getApplicationContext());
+                managerCompat.notify(999,builder.build());
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Override
@@ -158,7 +214,6 @@ public class NoticeActivity extends AppCompatActivity {
         NoticeAdapter.notifyDataSetChanged();
         NoticeAdapter.startListening();
     }
-
     @Override
     protected void onStop() {
         super.onStop();
